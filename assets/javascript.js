299c54d190aeea0bcf7f99f743def96a92541479
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2023-06-24 19:22:06 +0300

    Fix number of tiles not updating when changed

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,22 +1,22 @@
 const sketchpad = document.querySelector(".sketchpad");
 const tile = document.createElement("div");
 const drawButton = document.querySelector("#drawbutton");
+let tileSize = 16;
 
 drawButton.addEventListener("click", () => {
+  clear();
   draw();
 });
 
 tile.setAttribute("style", "background: white; width: 16px; height: 16px; border: 1px solid black");  
-const numberOfVerticalTiles = document.querySelector("#verticalsize").value;
-const numberOfHorizontalTiles = document.querySelector("#horizontalsize").value;
-const tileSize = 16;
 
 function draw() {
+  let numberOfVerticalTiles = document.querySelector("#verticalsize").value;
+  let numberOfHorizontalTiles = document.querySelector("#horizontalsize").value;
   let sketchpadVerticalSize = tileSize * numberOfVerticalTiles;
   let sketchpadHorizontalSize = tileSize * numberOfHorizontalTiles;
-  console.log("hor size in px: " + sketchpadHorizontalSize);
-  console.log("number of hor tiles: " + numberOfHorizontalTiles);
-  console.log("tylesize: " + tileSize)
+  console.log("horizontal size in px: " + sketchpadHorizontalSize);
+  console.log("number of columns: " + numberOfHorizontalTiles);
   for (let i = 0; i < numberOfHorizontalTiles * numberOfVerticalTiles; i++) {
         sketchpad.setAttribute("style", `height: ${sketchpadVerticalSize}px; width: ${sketchpadHorizontalSize}px`);
     sketchpad.appendChild(tile.cloneNode(true));
@@ -24,5 +24,9 @@ function draw() {
 };
 
 function clear() {
-  
+  while (sketchpad.hasChildNodes()) {
+    sketchpad.removeChild(sketchpad.firstChild)
+  }
+  document.querySelector("#verticalsize").textContent = "";
+  document.querySelector("#horizontalsize").textContent = "";
 };
