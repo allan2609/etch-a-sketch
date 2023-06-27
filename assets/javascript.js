2d5a00c0e25ca2b7614d2e86c24bd056c02b71b9
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2023-06-27 20:25:53 +0300

    Create function to draw squares

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,25 +1,22 @@
 const sketchpad = document.querySelector(".sketchpad");
-const tile = document.createElement("div");
 const drawButton = document.querySelector("#drawbutton");
-let tileSize = 16;
 
 drawButton.addEventListener("click", () => {
   clear();
   draw();
 });
 
-tile.setAttribute("style", "background: white; width: 16px; height: 16px; border: 1px solid black");  
-
 function draw() {
-  let numberOfVerticalTiles = document.querySelector("#verticalsize").value;
-  let numberOfHorizontalTiles = document.querySelector("#horizontalsize").value;
-  let sketchpadVerticalSize = tileSize * numberOfVerticalTiles;
-  let sketchpadHorizontalSize = tileSize * numberOfHorizontalTiles;
-  console.log("horizontal size in px: " + sketchpadHorizontalSize);
-  console.log("number of columns: " + numberOfHorizontalTiles);
-  for (let i = 0; i < numberOfHorizontalTiles * numberOfVerticalTiles; i++) {
-        sketchpad.setAttribute("style", `height: ${sketchpadVerticalSize}px; width: ${sketchpadHorizontalSize}px`);
-    sketchpad.appendChild(tile.cloneNode(true));
+  let numberOfSquares = document.querySelector("#squares-number").value;
+  for (let i = 0; i < numberOfSquares; i++) {
+    let column = document.createElement("div");
+    column.className = "column";
+    for (let j = 0; j < numberOfSquares; j++) {
+        let row = document.createElement("div");
+        row.className = "row";
+        column.appendChild(row);
+    }
+    sketchpad.appendChild(column);
   }
 };
 
@@ -27,6 +24,5 @@ function clear() {
   while (sketchpad.hasChildNodes()) {
     sketchpad.removeChild(sketchpad.firstChild)
   }
-  document.querySelector("#verticalsize").textContent = "";
-  document.querySelector("#horizontalsize").textContent = "";
+  document.querySelector("#squares-number").textContent = "";
 };
