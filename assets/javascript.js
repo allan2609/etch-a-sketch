3c3d360f8fe38ffbb71a38f6f6124b5b67b7fd20
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2023-07-01 23:49:50 +0300

    Create optional popup for size entry

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,21 +1,29 @@
 const sketchpad = document.querySelector(".sketchpad");
 const drawButton = document.querySelector("#drawbutton");
-const size = document.querySelector("#squares-output");
+let size = document.querySelector("#squares-output");
 const slider = document.querySelector("#squares-number");
+const modalContent = document.querySelector(".modal-content");
+const blurBg = document.querySelector(".blur-bg");
+const popup = document.querySelector(".squares-popup");
 
 drawButton.addEventListener("click", () => {
   clear();
   draw();
 });
 
+size.addEventListener("click", sizeEntry);
+
 slider.addEventListener("input", () => {
   const userSelection = document.querySelector("#squares-number").value;
-  size.textContent = `Size: ${userSelection}`;
+  document.querySelector("#squares-output").textContent = `Size: ${userSelection}`;
 });
 
+popup.addEventListener("change", readSize);
+
 function draw() {
+  console.log("Start function draw");
   const numberOfSquares = document.querySelector("#squares-number").value;
-  size.textContent = `Size: ${numberOfSquares}`;
+  document.querySelector("#squares-output").textContent = `Size: ${numberOfSquares}`;
   for (let i = 0; i < numberOfSquares; i++) {
     const column = document.createElement("div");
     column.className = "column";
@@ -34,9 +42,10 @@ function draw() {
 };
 
 function createBorders() {
+  console.log("Start function createBorders");
   const squares = document.querySelectorAll(".row");
-  const size = squares.length;
-  for (let i = 0; i < size; i++) {
+  const squaresCount = squares.length;
+  for (let i = 0; i < squaresCount; i++) {
     let box = squares[i];
     box.style.borderRight = ("1px solid black");
     box.style.borderBottom = ("1px solid black");
@@ -46,6 +55,7 @@ function createBorders() {
 };
 
 function paint(e) {
+  console.log("Start function paint(e)");
   const opacity = Number(e.target.style.opacity);
   const color = document.querySelector("#color").value;
   if (opacity > 0 && color === "#000000") {
@@ -62,9 +72,54 @@ function paint(e) {
 };
 
 function clear() {
+  console.log("Start function clear");
   while (sketchpad.hasChildNodes()) {
     sketchpad.removeChild(sketchpad.firstChild);
   }
 };
+
+function sizeEntry() {
+  console.log("Start function sizeEntry");
+  const userInput = document.querySelector(".squares-popup");
+  modalContent.classList.remove("hidden-modal");
+  blurBg.classList.remove("hidden-blur");
+  window.setTimeout(() => userInput.focus(), 0);
+};
+
+function readSize() {
+  console.log("Start function readSize");
+  const userInput = document.querySelector(".squares-popup");
+  if (userInput.value > 100) {
+    console.log("readSize = 100");
+    size = 100;
+    updateSize();
+  } else if (userInput.value < 2) {
+    console.log("readSize = 2");
+    size = 2;
+    updateSize();
+  } else if (userInput.value >= 2 && userInput.value <= 100) {
+    size = Number(userInput.value);
+    console.log("readSize = " + Number(userInput.value))
+    updateSize();
+  } else {
+    console.log("Error");
+  }
+};
+
+function updateSize() {
+  console.log("Start function updateSize");
+  console.log("Updated size is: " + size)
+  slider.textContent = size;
+  slider.value = size;
+  document.querySelector("#squares-output").textContent = `Size: ${size}`;
+};
+
+function closeModalFunction() {
+  console.log("Start function closeModal");
+  modalContent.classList.add("hidden-modal");
+  blurBg.classList.add("hidden-blur");
+};
+
+blurBg.addEventListener("click", closeModalFunction);
 
 draw();
