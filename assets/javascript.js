ffa857f7d59af364c60466ab7a96e5457d35c781
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2023-07-02 19:39:09 +0300

    Fix Safari not focusing

--- assets/javascript.js
+++ assets/javascript.js
@@ -83,7 +83,11 @@ function sizeEntry() {
   const userInput = document.querySelector(".squares-popup");
   modalContent.classList.remove("hidden-modal");
   blurBg.classList.remove("hidden-blur");
-  window.setTimeout(() => userInput.focus(), 0);
+  if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
+    safariFocus();
+  } else {
+    window.setTimeout(() => userInput.focus(), 0);
+  }
 };
 
 function readSize() {
@@ -99,7 +103,7 @@ function readSize() {
     updateSize();
   } else if (userInput.value >= 2 && userInput.value <= 100) {
     size = Number(userInput.value);
-    console.log("readSize = " + Number(userInput.value))
+    console.log("readSize = " + Number(userInput.value));
     updateSize();
   } else {
     console.log("Error");
@@ -118,6 +122,34 @@ function closeModalFunction() {
   console.log("Start function closeModal");
   modalContent.classList.add("hidden-modal");
   blurBg.classList.add("hidden-blur");
+};
+
+function safariFocus() {
+
+  // create invisible dummy input to receive the focus first
+  const fakeInput = document.createElement("input");
+  fakeInput.setAttribute("type", "text");
+  fakeInput.style.position = "absolute";
+  fakeInput.style.opacity = 0;
+  fakeInput.style.height = 0;
+  fakeInput.style.fontSize = "16px"; // disable auto zoom
+
+  // you may need to append to another element depending on the browser's auto 
+  // zoom/scroll behavior
+  document.body.prepend(fakeInput);
+
+  // focus so that subsequent async focus will work
+  fakeInput.focus();
+
+  setTimeout(() => {
+
+    // now we can focus on the target input
+    window.setTimeout(() => userInput.focus(), 0);
+
+    // cleanup
+    fakeInput.remove();
+    
+  }, 1000);
 };
 
 blurBg.addEventListener("click", closeModalFunction);
